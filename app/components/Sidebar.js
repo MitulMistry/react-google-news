87f3670ffd04c785f752a96c9806ee22fa0d39b2
MitulMistry <MitulMistryDev@gmail.com>
2017-10-26 17:35:43 -0500

    Add placeholder Sidebar links

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import { Nav, NavItem, NavLink } from 'reactstrap';
 
 export class Sidebar extends React.Component {
   render() {
     return (
-      <h3>Sidebar</h3>
+      <div>
+        <h4>Google News</h4>
+        <Nav vertical>
+          <NavItem>
+            <NavLink href="#">Top Stories</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#">World</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#">US</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#">Business</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#">Technology</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#">Entertainment</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#">Sports</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#">Science</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#">Health</NavLink>
+          </NavItem>
+        </Nav>
+      </div>
     );
   }
 }
