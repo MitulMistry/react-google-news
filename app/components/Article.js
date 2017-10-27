f4c53204db18c92b58dd8865a94f10dc66b330c8
MitulMistry <MitulMistryDev@gmail.com>
2017-10-26 20:25:39 -0500

    Set up placeholder Article layout

--- app/components/Article.js
+++ app/components/Article.js
@@ -3,7 +3,10 @@ import React from 'react';
 export class Article extends React.Component {
   render() {
     return (
-      <p>Article</p>
+      <div>
+        <h3>Article Title</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ante eros, sollicitudin at eros ut, varius eleifend arcu. Morbi tempor quam nec vestibulum accumsan. Proin finibus at justo a egestas.</p>
+      </div>
     );
   }
 }
--- app/components/ArticlesContainer.js
+++ app/components/ArticlesContainer.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
 import { Article } from './Article';
 
 export class ArticlesContainer extends React.Component {
   render() {
     return (
       <div>
-      <h4>ArticlesContainer</h4>
-      <Article />
-      <Article />
+        <Row>
+        <Col>
+          <Article />
+        </Col>
+        <Col>
+          <Article />
+        </Col>
+        </Row>
+        <Row>
+        <Col>
+          <Article />
+        </Col>
+        <Col>
+          <Article />
+        </Col>
+        </Row>
       </div>
     );
   }
