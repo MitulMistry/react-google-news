c1bb1d84cf7e13c33be61e68a3c7dcef549e14d5
MitulMistry <MitulMistryDev@gmail.com>
2017-10-27 22:25:15 -0500

    Set up API call with fetch

--- app/components/ArticlesContainer.js
+++ app/components/ArticlesContainer.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import 'whatwg-fetch'; //use fetch for API call
 import { Container, Row, Col } from 'reactstrap';
 import { Article } from './Article';
 
 export class ArticlesContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      articles: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://newsapi.org/v1/articles?source=google-news&sortBy=top&apiKey=') //needs API key
+      .then(function(response) {
+        return response.json();
+      }).then(function(json) {
+        const posts = json.articles; //array of article objects/hashes
+        this.setState({ articles });
+        console.log('parsed json', posts);
+      }).catch(function(ex) {
+        console.log('parsing failed', ex);
+      });
+  }
+
   render() {
     return (
       <div>
--- app/components/Footer.js
+++ app/components/Footer.js
@@ -9,7 +9,8 @@ export class Footer extends React.Component {
         <p>
           Website &copy; Copyright Mitul Mistry { currentYear } <br />
           All other materials copyright their respective holders <br />
-          <a href="https://github.com/MitulMistry/react-google-news" target="_blank">Github repo for this site</a>
+          <a href="https://github.com/MitulMistry/react-google-news" target="_blank">Github repo for this site</a><br />
+          <a href="https://newsapi.org/" target="_blank">Powered by News API</a>
         </p>
       </div>
     );
