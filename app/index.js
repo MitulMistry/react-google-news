92055a3fd13c7b210bf11d88a9def26954e18868
MitulMistry <MitulMistryDev@gmail.com>
2017-11-01 23:01:36 -0500

    Add background image and update styling to match

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css'; //for Webpack
 import './css/App.css';
+import bgImg from './images/background_01.jpg';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
