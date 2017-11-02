168076169715dff0cfed2b892417a698b268c633
MitulMistry <MitulMistryDev@gmail.com>
2017-11-01 22:57:09 -0500

    Add file-loader for Webpack image loading

--- webpack.config.js
+++ webpack.config.js
@@ -15,9 +15,21 @@ module.exports = {
         loader: 'babel-loader'
       },
       {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images/'
+            }
+          }
+        ]
+      },
+      {
         test: /\.css$/,
-        loader: "style-loader!css-loader"
-      }
+        loader: "style-loader!css-loader?url=false"
+      },
     ]
   },
   output: {
