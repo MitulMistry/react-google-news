b6eac970a6da98e7c6527cf16f990b3ec0132265
MitulMistry <MitulMistryDev@gmail.com>
2017-11-03 19:44:40 -0500

    Extract query string generation for Express API routes to module

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express'); //https://github.com/heroku/node-js-getting-started/blob/master/index.js
 var cors = require('cors');
 var request = require('request');
+
+var newsQuery = require('./lib/newsQuery.js'); //helper functions for generating queries
+
 var app = express();
 
 if (process.env.NODE_ENV !== 'production') {
@@ -17,48 +20,44 @@ app.get('/', function(req, res) { //request, response
 });
 
 // API ---
-const API_KEY = '&apiKey=' + process.env.API_KEY; //load API key from environment
-const API_URL = 'https://newsapi.org/v1/articles?source=';
-const SORT_TOP = '&sortBy=top';
-
 app.get('/api/top', cors(), function(req, res) {
-  req.pipe(request(API_URL + 'google-news' + SORT_TOP + API_KEY)).pipe(res);
+  req.pipe(request(newsQuery.sourceByTop('google-news'))).pipe(res);
 });
 
 app.get('/api/arstechnica', cors(), function(req, res) {
-  req.pipe(request(API_URL + 'ars-technica' + SORT_TOP + API_KEY)).pipe(res);
+  req.pipe(request(newsQuery.sourceByTop('ars-technica'))).pipe(res);
 });
 
 app.get('/api/associatedspress', cors(), function(req, res) {
-  req.pipe(request(API_URL + 'associated-press' + SORT_TOP + API_KEY)).pipe(res);
+  req.pipe(request(newsQuery.sourceByTop('associated-press'))).pipe(res);
 });
 
 app.get('/api/cnn', cors(), function(req, res) {
-  req.pipe(request(API_URL + 'cnn' + SORT_TOP + API_KEY)).pipe(res);
+  req.pipe(request(newsQuery.sourceByTop('cnn'))).pipe(res);
 });
 
 app.get('/api/espn', cors(), function(req, res) {
-  req.pipe(request(API_URL + 'google-news' + SORT_TOP + API_KEY)).pipe(res);
+  req.pipe(request(newsQuery.sourceByTop('espn'))).pipe(res);
 });
 
 app.get('/api/fortune', cors(), function(req, res) {
-  req.pipe(request(API_URL + 'fortune' + SORT_TOP + API_KEY)).pipe(res);
+  req.pipe(request(newsQuery.sourceByTop('fortune'))).pipe(res);
 });
 
 app.get('/api/reuters', cors(), function(req, res) {
-  req.pipe(request(API_URL + 'reuters' + SORT_TOP + API_KEY)).pipe(res);
+  req.pipe(request(newsQuery.sourceByTop('reuters'))).pipe(res);
 });
 
 app.get('/api/techcrunch', cors(), function(req, res) {
-  req.pipe(request(API_URL + 'techcrunch' + SORT_TOP + API_KEY)).pipe(res);
+  req.pipe(request(newsQuery.sourceByTop('techcrunch'))).pipe(res);
 });
 
 app.get('/api/wallstreetjournal', cors(), function(req, res) {
-  req.pipe(request(API_URL + 'the-wall-street-journal' + SORT_TOP + API_KEY)).pipe(res);
+  req.pipe(request(newsQuery.sourceByTop('the-wall-street-journal'))).pipe(res);
 });
 
 app.get('/api/time', cors(), function(req, res) {
-  req.pipe(request(API_URL + 'time' + SORT_TOP + API_KEY)).pipe(res);
+  req.pipe(request(newsQuery.sourceByTop('time'))).pipe(res);
 });
 
 app.listen(app.get('port'), function() {
