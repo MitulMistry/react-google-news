cb7fa42d64e1cfaae0ecf1e055f81552ad8a0c10
MitulMistry <MitulMistryDev@gmail.com>
2017-11-09 12:31:14 -0600

    Refactor lifecycle methods for article API calls

--- app/components/ArticlesContainer.js
+++ app/components/ArticlesContainer.js
@@ -14,9 +14,28 @@ export class ArticlesContainer extends React.Component {
   }
 
   componentDidMount() {
-    this.setState({ loading: true }); //necessary for method reuse?
+    var apiRoute = this.getApiRoute(this.props.location.pathname);
+    this.getArticles(apiRoute); //get articles for first time being mounted
+  }
 
-    fetch('/api/top') //requests API from back end (server.js)
+  componentWillReceiveProps(nextProps) {
+    var route = nextProps.location.pathname; //get the incoming route (From React Router)
+
+    if (route !== this.props.location.pathname) { //compare incoming route to previous route
+      var apiRoute = this.getApiRoute(route);
+      this.getArticles(apiRoute);
+    }
+  }
+
+  getApiRoute(route) {
+    var apiRoute = (route === '/') ? '/api/top' : '/api' + route; //default route should make API call to api/top, otherwise use route name for API route (e.g. "espn" becomes ""/api/espn")
+    return apiRoute;
+  }
+
+  getArticles(route) {
+    this.setState({ loading: true }); //necessary for method reuse
+
+    fetch(route) //requests API from back end (server.js)
       .then(response => {
         return response.json();
       }).then(json => {
@@ -26,34 +45,6 @@ export class ArticlesContainer extends React.Component {
       }).catch(ex => {
         console.log('parsing failed', ex);
       });
-  }
-
-  componentWillReceiveProps(nextProps) {
-    var route = nextProps.location.pathname; //get the incoming route
-
-    if (route !== this.props.location.pathname) { //compare incoming route to previous route
-      this.setState({ loading: true }); //necessary for method reuse?
-      console.log(route);
-
-      var apiRoute;
-
-      if (route === '/') {
-        apiRoute = '/top'; //default route should make API call to /top
-      } else {
-        apiRoute = '/api' + route;
-      }
-
-      fetch(apiRoute) //requests API from back end (server.js)
-        .then(response => {
-          return response.json();
-        }).then(json => {
-          const articles = json.articles; //array of article objects/hashes
-          this.setState({ articles: articles, loading: false });
-          console.log('parsed json', this.state.articles);
-        }).catch(ex => {
-          console.log('parsing failed', ex);
-        });
-    }
   }
 
   render() {
