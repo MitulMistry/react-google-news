a2077dd1367cbc5c549b2bfa8dba1e4ed0558b07
MitulMistry <MitulMistryDev@gmail.com>
2018-09-07 18:01:32 -0500

    Add lazy load fade in for Article components

--- app/components/Article.js
+++ app/components/Article.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import FadeIn from 'react-lazyload-fadein';
 import { Container, Row, Col, Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle } from 'reactstrap';
 
 export class Article extends React.Component {
   render() {
     return (
-      <Card className="article">
-        <a href={this.props.url} target="_blank"><CardImg top width="100%" src={this.props.urlToImage} alt="Article image" /></a>
-        <CardBody>
-          <CardTitle><a href={this.props.url} target="_blank">{this.props.title}</a></CardTitle>
-          <CardText>{this.props.description}</CardText>
-        </CardBody>
-      </Card>
+      <FadeIn height={500}>
+        {onload => (
+          <Card className="article">
+            <a href={this.props.url} target="_blank"><CardImg top width="100%" src={this.props.urlToImage} onLoad={onload} alt="Article image" /></a>
+            <CardBody>
+              <CardTitle><a href={this.props.url} target="_blank">{this.props.title}</a></CardTitle>
+              <CardText>{this.props.description}</CardText>
+            </CardBody>
+          </Card>
+        )}
+      </FadeIn>
     );
   }
 }
