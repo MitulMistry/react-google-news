b230dab58613bb2d12b09988b4ea5ceb64e6d239
MitulMistry <MitulMistryDev@gmail.com>
2018-09-27 13:43:37 -0500

    Reorder webpack.prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,25 +2,11 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const extractSass = new MiniCssExtractPlugin({
-    filename: '[name].[hash].css',
-    chunkFilename: '[id].[hash].css'
-});
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
-  module: {
-    rules: [{
-      test: /\.scss$/,
-      use: [
-        MiniCssExtractPlugin.loader, // extract css to separate file
-        'css-loader?url=false',
-        'sass-loader'
-      ]
-    }]
-  },
   plugins: [
     new UglifyJSPlugin({
       sourceMap: true
@@ -30,6 +16,19 @@ module.exports = merge(common, {
         'NODE_ENV': JSON.stringify('production') // specify production environment
       }
     }),
-    extractSass
-  ]
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash].css',
+      chunkFilename: '[id].[hash].css'
+    })
+  ],
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [
+        MiniCssExtractPlugin.loader, // extract css to separate file
+        'css-loader?url=false',
+        'sass-loader'
+      ]
+    }]
+  }  
 });
